ee9684528393e8e0abce0e98989e3873e1751f3c
NuhYlmz <senturknuhyilmaz@gmail.com>
2022-03-20 15:54:50 +0300

    products listed

--- client/app/public/pages/manageShop/index.js
+++ client/app/public/pages/manageShop/index.js
@@ -1,6 +1,8 @@
 Template.publicPageManageShop.onCreated(function () {
     this.state = new ReactiveDict(null, {
         categories: [],
+        products: [],
+        i:0,
     });
 
 });
@@ -20,23 +22,43 @@ Template.publicPageManageShop.onRendered(function () {
             console.log(result);
             self.state.set('categories', result.categories);
         });
+        Meteor.call('product.list', {}, function (error, result) {
+
+            if (error) {
+                console.log(error);
+                return;
+            }
+
+            console.log(result);
+            self.state.set('products', result.products);
+        });
     });
 });
 
 Template.publicPageManageShop.helpers({
-
+    counter: function() {
+        Template.instance().i++;
+        return Template.instance().i;
+      }
 });
 
 Template.publicPageManageShop.events({
     'submit form#brdCategoryCreateForm': function (event, template) {
         event.preventDefault();
-        const name = event.target.category.value;
-        const obj = {
+        const name = event.target.productNameInput.value;
+        const parentId = event.target.categoryParentSelect.value;
+        const obj = parentId=="default" ? {
             category: {
                 name: name
             }
-        }
-
+        } : 
+        {
+            category: {
+                name: name,
+                parentId: parentId
+            }
+        };
+        console.log(obj);
         Meteor.call('category.create', obj, function (error, result) {
 
             if (error) {
@@ -72,7 +94,7 @@ Template.publicPageManageShop.events({
     },
     'submit form#brdProductCreateForm': function (event, template) {
         event.preventDefault();
-        
+
         const name = event.target.productNameInput.value;
 
         const categoryId = event.target.productCategorySelect.value;
